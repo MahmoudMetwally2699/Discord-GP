bc1f463778d3e118b9e675baf09bd69d28b5bb8d
MahmoudMetwally2699 <113688967+MahmoudMetwally2699@users.noreply.github.com>
2023-10-21 17:48:50 +0300

    Updates server.js

--- server/server.js
+++ server/server.js
@@ -30,6 +30,11 @@ const port = process.env.PORT || process.env.API_PORT || 5000;
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://discord-gp-frontend.vercel.app/');
+  next();
+});
+
 
 app.use(
   cors({
