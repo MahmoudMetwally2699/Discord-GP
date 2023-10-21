672933d06802c86fe747779b7b3afc4e399fb01b
MahmoudMetwally2699 <113688967+MahmoudMetwally2699@users.noreply.github.com>
2023-10-21 21:59:01 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ require("dotenv").config();
 const helmet = require("helmet");
 const xss = require("xss-clean");
 const mongoSanitize = require("express-mongo-sanitize");
+
 //Database Connection
 const connectDB = require("./db/connection");
 
@@ -27,21 +28,10 @@ const socketServer = require("./socketServer");
 const port = process.env.PORT || process.env.API_PORT || 5000;
 
 const app = express();
-app.use(
-  cors({
-    origin: "https://discord-gp-frontend.vercel.app/",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
-});
-
 
 app.use(express.json());
+
+app.use(cors());
 app.use(helmet());
 app.use(xss());
 app.use(mongoSanitize());
